cf2a10fb0522e1fdb36b56e7ec9e49c4dee996a5
gbarreiro <gonzalo.barreiro@mercadolibre.com>
2024-02-28 03:10:27 -0300

    Add login background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'login': "url('/images/login-bg.png')",
+      },
+  }
   },
   plugins: [],
 }
